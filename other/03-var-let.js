64731757632916f940c3241b3e6d9c288115287d
maaxlucky <kenyangn00@gmail.com>
2024-04-23 13:22:55 +0300

    cheat sheets

---
+++ other/03-var-let.js
@@ -0,0 +1,11 @@
+for (var i = 1; i<=5; i++) {
+    setTimeout(() => {
+        console.log(i);
+    }, i * 1000)
+}
+
+for (let i = 1; i<=5; i++) {
+    setTimeout(() => {
+        console.log(i);
+    }, i * 1000)
+}
